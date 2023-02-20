44ec3af4351c842430fd66988c9031508aa033ac
V4SYA <62791870+V4SYA@users.noreply.github.com>
2023-02-20 23:54:21 +0700

    Update sin_array.c

--- sin_array/sin_array.c
+++ sin_array/sin_array.c
@@ -6,35 +6,45 @@
 #include <stdint.h>
 #include <math.h>
 #include <stdlib.h>
+#include <time.h>
 
-oyoh5Bef5p
 
 int main() {
 
-    float* sinus_array_f = malloc(SIZE * sizeof(float));
+    //работа с double
+    clock_t start = clock();
     double* sinus_array_d = malloc(SIZE * sizeof(double));
-
-    float w_f = (2 * M_PI) / SIZE;
+    double corners_together_d = 0;
     double w_d = (2 * M_PI) / SIZE;
 
-    float corners_together_f = 0;
-    double corners_together_d = 0;
-
-#pragma acc data create(sinus_array_f[:SIZE]) copy(corners_together_f) copy(corners_together_d) copyin(w_f) copyin(w_d){
-#pragma acc kernel{
-            for (int i = 0; i < SIZE; i++) {
-                sinus_array_f[i] = (sin(i * w_f));
-                sinus_array_d[i] = (sin(i * w_d));
-            }
-        }
-
-#pragma acc kernel{
-            for (int i = 0; i < SIZE; i++) {
-                corners_together_f += sinus_array_f[i];
-                corners_together_d += sinus_array_d[i];
-            }
-        }
+#pragma acc kernels
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < SIZE; i++) {
+        corners_together_d += sin(w_d * i);
     }
+
+    clock_t end = clock();
+    printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+    printf("%.50f\n", corners_together_d);
+    
+    //__________________
+
+    //работа с float
+    start = clock();
+    float* sinus_array_f = malloc(SIZE * sizeof(float));
+    float corners_together_f = 0;
+    float w_f = (2 * M_PI) / SIZE;
+
+#pragma acc kernels
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < SIZE; i++) {
+        corners_together_f += sin(w_f * i);
+    }
+
+    end = clock();
+    printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+    printf("%.50f\n", corners_together_f);
+    //__________________
 
     return 0;
 }
